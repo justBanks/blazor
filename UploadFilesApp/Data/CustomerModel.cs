4eabfa9be368630dc2018212d2b2460e6e97f22f
Darryl Banks <277740+justBanks@users.noreply.github.com>
2023-04-28 06:13:10 -0700

    got it

---
+++ UploadFilesApp/Data/CustomerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UploadFilesApp.Data;
+
+public class CustomerModel
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = "tcorey";
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FileName { get; set; }
+}
